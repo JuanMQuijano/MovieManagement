6262523849455db9ede92baf97c6292b607c51d7
JuanMQuijano <jmquijano@unimayor.edu.co>
2024-09-10 17:12:30 -0500

    Logro: Creación de entidades y sus relaciones: Movie, Rating, User

---
+++ src/main/java/org/jquijano/proyects/MovieManagement/persistence/entity/Rating.java
@@ -0,0 +1,80 @@
+package org.jquijano.proyects.MovieManagement.persistence.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Check;
+
+@Entity
+public class Rating {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "movie_id", nullable = false)
+    private Long movieId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    @Check(constraints = "rating >= 0 AND rating <= 5")
+    private int rating;
+
+    //insertable and updatable establece que los atributos no serán inyectados en la base de datos y solo son para traer datos
+    @ManyToOne
+    @JoinColumn(name = "movie_id", insertable = false, updatable = false)
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
